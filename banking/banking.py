f602d323b342090215e40a517af82898659d7285
Isaul <tiradolisaul@gmail.com>
2024-10-24 12:22:17 -0700

    generate pin function implemented

--- banking/banking.py
+++ banking/banking.py
@@ -1,13 +1,33 @@
-def menu():
-    pass
+from random import randint
+
+
+class MenuError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+def read_main_menu_option() -> int:
+    while True:
+        try:
+            menu_text = '1. Create an account\n2. Log into account\n0. Exit'
+            option = int(input(menu_text))
+            if option in (0, 1, 2):
+                return option
+            else:
+                raise MenuError("Option must be 0, 1 or 2. ")
+        except ValueError:
+            print("\nError: Enter a valid number.\n")
+        except MenuError as me:
+            print(f'Error: {me.message}')
 
 
 def create_an_account():
     pass
 
 
-def generate_pin():
-    pass
+def generate_pin() -> str:
+    return ''.join(str(randint(0, 9)) for _ in range(4))
 
 
 def create_card():
@@ -15,16 +35,21 @@ def create_card():
 
 
 class Card:
-    pass
+    def __init__(self, id: str, pin: str):
+        self.id = id
+        self.pin = pin
 
 
 def log_into_account():
     pass
 
 
-def main():
+def main() -> None:
+    pin = generate_pin()
+    print(pin)
     pass
 
 
 if __name__ == '__main__':
+    main()
     pass
