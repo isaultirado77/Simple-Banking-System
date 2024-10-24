2d8162fcf2339947e1c50fcca52cc762c1e90bdf
Isaul <tiradolisaul@gmail.com>
2024-10-24 12:00:52 -0700

    init repo

---
+++ banking/banking.py
@@ -0,0 +1,30 @@
+def menu():
+    pass
+
+
+def create_an_account():
+    pass
+
+
+def generate_pin():
+    pass
+
+
+def create_card():
+    pass
+
+
+class Card:
+    pass
+
+
+def log_into_account():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    pass
