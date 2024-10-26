17b4a4b180acd942bdbc3e62c06313a28957cd2c
Isaul <tiradolisaul@gmail.com>
2024-10-26 15:57:57 -0700

    card generator using luhn algorithm implemented: testing

--- banking/card.py
+++ banking/card.py
@@ -5,8 +5,51 @@ def generate_random_int_sequence(n: int = 5) -> str:
     return ''.join(str(randint(0, 9)) for _ in range(n))
 
 
+def drop_last_digit(num: int) -> int:
+    if num < 10:
+        return num
+    return num // 10
+
+
+def multiply_by_two(num: int) -> int:
+    if num % 2 == 0:
+        return num * 2
+    return num
+
+
+def subtract_nine(num: int) -> int:
+    if num > 9:
+        return num - 9
+    return num
+
+
+def get_uncompleted_id_sum(uncompleted_id: str) -> int:
+    summ = 0
+    for digit in uncompleted_id:
+        digit = int(digit)
+        digit = drop_last_digit(digit)
+        digit = multiply_by_two(digit)
+        digit = subtract_nine(digit)
+        summ += digit
+    return summ
+
+
+def get_control_number(uncompleted_id: str) -> int:
+    summ = get_uncompleted_id_sum(uncompleted_id)
+    if summ % 10 == 0:
+        return 0
+    last_digit = 0
+    while True:
+        if (summ + last_digit) % 10 == 0:
+            break
+        last_digit += 1
+    return last_digit
+
+
 def generate_card_id() -> str:
-    return '400000' + generate_random_int_sequence(10)
+    card_id = '400000' + generate_random_int_sequence(9)
+    control_number = get_control_number(card_id)
+    return card_id + str(control_number)
 
 
 def generate_pin() -> str:
