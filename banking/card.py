f47f4e59fd0ef1ea734c1a60b790353141445633
Isaul <tiradolisaul@gmail.com>
2024-10-26 16:37:50 -0700

    card generator using luhn algorithm implemented: fixed

--- banking/card.py
+++ banking/card.py
@@ -5,51 +5,28 @@ def generate_random_int_sequence(n: int = 5) -> str:
     return ''.join(str(randint(0, 9)) for _ in range(n))
 
 
-def drop_last_digit(num: int) -> int:
-    if num < 10:
-        return num
-    return num // 10
-
-
-def multiply_by_two(num: int) -> int:
-    if num % 2 == 0:
-        return num * 2
-    return num
-
-
-def subtract_nine(num: int) -> int:
-    if num > 9:
-        return num - 9
-    return num
-
-
-def get_uncompleted_id_sum(uncompleted_id: str) -> int:
-    summ = 0
-    for digit in uncompleted_id:
+def calculate_luhn_sum(uncompleted_id: str) -> int:
+    total_sum = 0
+    reversed_id = uncompleted_id[::-1]
+    for index, digit in enumerate(reversed_id):
         digit = int(digit)
-        digit = drop_last_digit(digit)
-        digit = multiply_by_two(digit)
-        digit = subtract_nine(digit)
-        summ += digit
-    return summ
+        if index % 2 == 0:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+        total_sum += digit
+    return total_sum
 
 
 def get_control_number(uncompleted_id: str) -> int:
-    summ = get_uncompleted_id_sum(uncompleted_id)
-    if summ % 10 == 0:
-        return 0
-    last_digit = 0
-    while True:
-        if (summ + last_digit) % 10 == 0:
-            break
-        last_digit += 1
-    return last_digit
+    total_sum = calculate_luhn_sum(uncompleted_id)
+    return (10 - (total_sum % 10)) % 10
 
 
 def generate_card_id() -> str:
-    card_id = '400000' + generate_random_int_sequence(9)
-    control_number = get_control_number(card_id)
-    return card_id + str(control_number)
+    base_id = '400000' + generate_random_int_sequence(9)
+    control_number = get_control_number(base_id)
+    return base_id + str(control_number)
 
 
 def generate_pin() -> str:
