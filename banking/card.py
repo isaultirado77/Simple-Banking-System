12cc1f58fa3a3f317e3a97f04ecc42f19fec5ab1
Isaul <tiradolisaul@gmail.com>
2024-10-25 19:55:58 -0700

    Card class implemtented

---
+++ banking/card.py
@@ -0,0 +1,28 @@
+from random import randint
+
+
+def generate_random_int_sequence(n: int = 5) -> str:
+    return ''.join(str(randint(0, 9)) for _ in range(n))
+
+
+def generate_card_id() -> str:
+    return '400000' + generate_random_int_sequence(10)
+
+
+def generate_pin() -> str:
+    return generate_random_int_sequence(4)
+
+
+class Card:
+    def __init__(self, id: str, pin: str):
+        self.id = id
+        self.pin = pin
+
+    @staticmethod
+    def generate_card() -> 'Card':
+        id = generate_card_id()
+        pin = generate_pin()
+        return Card(id, pin)
+
+    def __str__(self):
+        return f'Your card number:\n{self.id}\nYour card PIN:\n{self.pin}\n'
