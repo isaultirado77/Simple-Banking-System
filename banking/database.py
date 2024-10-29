fd12252cf4966180c2ffc46789d5c1f84cba2a4b
Isaul <tiradolisaul@gmail.com>
2024-10-28 19:42:21 -0700

    dabase.py implemented: need to conect with the main

--- banking/database.py
+++ banking/database.py
@@ -1,3 +1,33 @@
 import sqlite3
 
-connection = sqlite3.connect('card.s3db')
+CREATE_CARD_TABLE = "CREATE TABLE IF NOT EXISTS card (id INTEGER PRIMARY KEY, number TEXT, pin TEXT, balance INTEGER DEFAULT 0); "
+
+INSERT_CARD = "INSERT INTO card (id, number, pin, balance) VALUES (?, ?, ?, ?); "
+
+GET_ALL_CARDS = "SELECT * FROM card; "
+
+GET_CARD_BY_NUMBER = "SELECT * FROM card WHERE number = ?; "
+
+
+def connect():
+    return sqlite3.connect('card.s3db')
+
+
+def create_table(connection):
+    with connection: connection.execute(CREATE_CARD_TABLE)
+
+
+def add_card(connection, number, pin, balance):
+    with connection:
+        connection.execute(INSERT_CARD, (number, pin, balance))
+
+
+def get_all_cards(connection):
+    with connection:
+        return connection.execute(GET_ALL_CARDS).fetchall()
+
+
+def get_card_by_number(connection, number):
+    with connection:
+        return connection.execute(GET_CARD_BY_NUMBER, (number,)).fetchall()
+
